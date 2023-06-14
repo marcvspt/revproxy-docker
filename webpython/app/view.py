3062d6fb5c15686545ccde544845247bd0e8d8dd
Atriox Banished <78035694+atriox2510@users.noreply.github.com>
2023-06-14 15:33:28 -0600

    Update view.py

--- webpython/app/view.py
+++ webpython/app/view.py
@@ -3,11 +3,9 @@ import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
     return render_template('index.html')
-
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
