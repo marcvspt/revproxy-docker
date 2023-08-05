78f7ecfaa1a212901603a0ef8b7cd8519c43987a
marcvspt <78035694+marcvspt@users.noreply.github.com>
2023-08-05 10:20:37 -0600

    Update services

--- webnodejs/app/server.js
+++ webnodejs/app/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const path = require('path')
+const morgan = require('morgan');
 
-const port = process.env.NODEJS_PORT || 3000
+const PORT = process.env.NODEJS_PORT || 3000
 
 const app = express()
+app.disable('x-powered-by')
+
+app.use(morgan(':remote-addr - - [:date[clf]] ":method :url HTTP/:http-version" :status -'));
+
 const templatesDir = path.join(__dirname, 'templates')
 app.use(express.static(templatesDir))
 
@@ -11,6 +16,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(templatesDir, 'index.html'));
 })
 
-app.listen(port, () => {
-    console.log(`Running on http://0.0.0.0:${port}`)
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Running in all addresses http://0.0.0.0:${PORT}`)
 })
